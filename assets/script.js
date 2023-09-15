723252c9cdece851d1935ae246c8bbfe07cbc0b3
Paco <paco.refractando@gmail.com>
2023-09-15 10:21:10 +0200

    añadido JS para cambiar seccion about y correccion de bugs

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+
+let selectorAbout = document.querySelectorAll('.selector');
+let contenido = document.querySelectorAll('.container-content');
+
+
+
+cambiaAbout=function(){
+    selectorAbout.forEach((element)=>{
+        if(element.classList.contains('active')){
+            element.classList.remove('active');
+        }
+    })
+
+    contenido.forEach((element)=>{
+        if(element.classList.contains('active')){
+            element.classList.remove('active');
+        }
+    })
+
+    switch(this.id){
+        case 'selector-nosotros':
+            document.getElementById('content-nosotros').classList.add('active');
+            break;
+        case 'selector-shokunin':
+            document.getElementById('content-shokunin').classList.add('active');
+            break;
+        case 'selector-katul':
+            document.getElementById('content-katul').classList.add('active');
+            break;
+    }
+    
+    this.classList.add('active');
+}
+selectorAbout.forEach((element) => element.addEventListener('click', cambiaAbout));
+
+;
